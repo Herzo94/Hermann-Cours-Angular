bc903050f103b0a3dd7d8bbc2a90e3a632ed9f78
hermannmanuel <hermann.manuel>
2020-05-23 21:26:10 +0200

    Exo 05 done

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -8,23 +8,24 @@ import { Component, OnInit } from '@angular/core';
 
 
 export class ProductComponent implements OnInit {
-  //public searchProduct: string = '';
-  public imageIsShown: boolean = true; // display by default
+  public imageIsShown: boolean = true; // display (true) by default
   public noProductTemplate: string = 'Pas de tableau';
   public searchText: string = 'hallo';
 
   //Fonction appelée lors du clic
   toggleImage() {
-
-    this.imageIsShown = ! this.imageIsShown;
-    
+      this.imageIsShown =! this.imageIsShown; // n'est pas égal donc l'inverse de true et vice versa
     }
 
-  //constructor() { } // mettre ce bout de code en commentaire car il sert à rien n'est-ce pas ? 
+  //constructor() { } // mettre ce bout de code en commentaire car il sert à rien n'est-ce pas ??????? 
 
   ngOnInit(): void {}
 
-  searchProduct: string;
+  searchProduct: string; // Cette déclaration pas nécéssaire on dirait ?????????
+  //Question GIT -> Comment publier les modifs réalisé en local sur le master du site github afin de voir les modifications directement depuis le site (J'avais déjà posé cette question)
+  //-> $ Git rebase master ??
+  //Realision des quelques exercies sur le site de git jusqu'au -> git rebase master
+  //+ Réaliser les commits directement depuuis le terminal après avoir -> géré les states changed
 
   public products: Product[] = [
     {
