0f8c96709c5ef471f2ed0e7de8e529d8c7f51b3e
hermannmanuel <hermann.manuel>
2020-05-26 09:44:39 +0200

    Notation fonctionnelle

--- src/app/shared/star/star.component.ts
+++ src/app/shared/star/star.component.ts
@@ -7,8 +7,15 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 })
 export class StarComponent implements OnInit {
 
-  @Input() rating : number = 4; //1. déclaration de la variable dans le fichier .ts
-  @Output() ratingClicked: EventEmitter<number> = new EventEmitter<number>()
+  //Input et Output ok!
+  
+  //Pas réussir à afficher les étoiles
+  
+  //eventmitter -> Interface, Class dispo par Angular -> Package oû il y a les methodes essentielles à angular pas clair non plus
+  //Problème au clique
+
+  @Input() rating : number = 4;  //Argument optionnel // -> Bouton configuration
+  @Output() ratingClicked: EventEmitter<number> = new EventEmitter<number>() //Notifie quand il y a un changement, output return de la fonction / Sonnerie
 
   constructor() { }
 
